c12d36bc59086cc0529417e03e5090df9e80a4e9
Harmary <maria.h4rlamova@yandex.ru>
2023-08-04 16:03:04 +0300

    applied fsd pattern

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from "react-router-dom";
-import './styles/index.scss'
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import './styles/index.scss';
 import { Suspense } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames";
+import { useTheme } from "app/providers/ThemeProviders";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 
 
@@ -18,8 +18,8 @@ const App = () => {
             <Link to={'/'}>Main</Link>
             <Suspense fallback={<div>Loading</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />}></Route>
-                    <Route path={'/'} element={<MainPageAsync />}></Route>
+                    <Route path={'/about'} element={<AboutPage />}></Route>
+                    <Route path={'/'} element={<MainPage />}></Route>
 
                 </Routes>
             </Suspense>
