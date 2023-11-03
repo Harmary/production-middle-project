caffa75f69382306b11af37e13b37c17c4934c84
Harmary <maria.h4rlamova@yandex.ru>
2023-11-03 11:34:40 +0300

    added ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import './styles/index.scss';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProviders';
 import { Navbar } from 'widgets/navbar';
