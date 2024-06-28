1efd83749f20e3c945faf0db401827200794d61e
Maria Kharlamova <maria.h4rlamova@gmail.com>
2024-06-28 11:15:28 +0300

    added json-server

--- src/entities/Counter/ui/Counter.test.ts
+++ src/entities/Counter/ui/Counter.test.ts
@@ -5,8 +5,8 @@ import { Counter } from '..';
 describe('Counter', () => {
     test('with only first param', () => {
         componentRender(
-            Counter,
-            {
+            <Counter/>
+            ,{
                 initialState: {
                     counter: { value: 10 },
                 },
@@ -16,8 +16,8 @@ describe('Counter', () => {
     });
     test('increment', () => {
         componentRender(
-            Counter,
-            {
+            <Counter/>
+            ,{
                 initialState: {
                     counter: { value: 10 },
                 },
@@ -28,8 +28,8 @@ describe('Counter', () => {
     });
     test('decrement', () => {
         componentRender(
-            Counter,
-            {
+            <Counter/>
+            ,{
                 initialState: {
                     counter: { value: 10 },
                 },
